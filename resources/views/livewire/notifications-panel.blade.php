f9d81f2d1d8dae783c447969d2e021d5560b0450
Gerard <41292403+gerardpaardenkoper@users.noreply.github.com>
2024-04-22 13:53:57 +0200

    Added support for new accounts

--- resources/views/livewire/notifications-panel.blade.php
+++ resources/views/livewire/notifications-panel.blade.php
@@ -1,8 +1,14 @@
-<div class="space-y-4">
-    @foreach($notificationTypes as $notificationType)
-        <button type="button" class="w-full py-2 px-4 rounded-lg bg-gray-100 flex items-center justify-start gap-4" wire:key="{{ $notificationType->id }}" wire:click="toggleCheckOff({{ $notificationType->id }})">
-            <input type="checkbox" id="notification-{{ $notificationType->id }}" class="rounded-lg border border-black" {{ $this->isNotificationChecked($notificationType->id) ? 'checked' : '' }}>
-            <strong class="{{ $this->isNotificationChecked($notificationType->id) ? 'line-through' : '' }}">{{ $notificationType->name }}</strong>
-        </button>
-    @endforeach
+<div>
+    @if($notificationTypes->isEmpty())
+        <p>Je hebt op dit moment geen meldingen ingesteld, voeg er een paar toe via het klokje linksboven.</p>
+    @else
+        <div class="space-y-4">
+            @foreach($notificationTypes as $notificationType)
+                <button type="button" class="w-full py-2 px-4 rounded-lg bg-gray-100 flex items-center justify-start gap-4" wire:key="{{ $notificationType->id }}" wire:click="toggleCheckOff({{ $notificationType->id }})">
+                    <input type="checkbox" id="notification-{{ $notificationType->id }}" class="rounded-lg border border-black" {{ $this->isNotificationChecked($notificationType->id) ? 'checked' : '' }}>
+                    <strong class="{{ $this->isNotificationChecked($notificationType->id) ? 'line-through' : '' }}">{{ $notificationType->name }}</strong>
+                </button>
+            @endforeach
+        </div>
+    @endif
 </div>
