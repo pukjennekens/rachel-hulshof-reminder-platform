ab0cb5874b9125c84be9e3a0d2a114bd705686d0
Gerard <41292403+gerardpaardenkoper@users.noreply.github.com>
2024-04-22 13:51:36 +0200

    Notifications panel UX adjustments

--- resources/views/livewire/notifications-panel.blade.php
+++ resources/views/livewire/notifications-panel.blade.php
@@ -1,8 +1,8 @@
 <div class="space-y-4">
     @foreach($notificationTypes as $notificationType)
-        <div class="py-2 px-4 rounded-lg bg-gray-100 flex items-center justify-start gap-4" wire:key="{{ $notificationType->id }}">
-            <input type="checkbox" id="notification-{{ $notificationType->id }}" class="rounded-lg border border-black" wire:click="toggleCheckOff({{ $notificationType->id }})" {{ $this->isNotificationChecked($notificationType->id) ? 'checked' : '' }}>
-            <strong>{{ $notificationType->name }}</strong>
-        </div>
+        <button type="button" class="w-full py-2 px-4 rounded-lg bg-gray-100 flex items-center justify-start gap-4" wire:key="{{ $notificationType->id }}" wire:click="toggleCheckOff({{ $notificationType->id }})">
+            <input type="checkbox" id="notification-{{ $notificationType->id }}" class="rounded-lg border border-black" {{ $this->isNotificationChecked($notificationType->id) ? 'checked' : '' }}>
+            <strong class="{{ $this->isNotificationChecked($notificationType->id) ? 'line-through' : '' }}">{{ $notificationType->name }}</strong>
+        </button>
     @endforeach
 </div>
