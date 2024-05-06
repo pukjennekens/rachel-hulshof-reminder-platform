175600aafeb34939b590a4d51201b3c1b08cb3f0
Gerard <41292403+gerardpaardenkoper@users.noreply.github.com>
2024-05-06 14:18:18 +0200

    Adjusted serviceworker

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -14,3 +14,18 @@ firebase.initializeApp({
 });
 
 const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function (payload) {
+    console.log(
+        "[firebase-messaging-sw.js] Received background message ",
+        payload
+    );
+
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+        body: payload.notification.body,
+        icon: payload.notification.icon,
+    };
+
+    self.registration.showNotification(notificationTitle, notificationOptions);
+});
