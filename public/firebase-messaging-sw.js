cece040dbd31b4e7bf4602a8318c1a5d576e2637
Gerard <41292403+gerardpaardenkoper@users.noreply.github.com>
2024-06-06 16:47:07 +0200

    Added a test

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -13,29 +13,4 @@ firebase.initializeApp({
     appId: "1:324732111099:web:6ae241d6dbe33aae16485e",
 });
 
-const messaging = firebase.messaging();
-
-self.addEventListener("notificationclick", function (event) {
-    event.notification.close();
-});
-
-messaging.onBackgroundMessage(function (payload) {
-    console.log(
-        "[firebase-messaging-sw.js] Received background message ",
-        payload
-    );
-
-    // Send the data to: https://webhook.site/adf13c92-be16-470e-baf5-7d323e5e1592
-    fetch("https://webhook.site/adf13c92-be16-470e-baf5-7d323e5e1592", {
-        method: "POST",
-        body: JSON.stringify(payload),
-    });
-
-    const notificationTitle = payload.data.title;
-    const notificationOptions = {
-        body: payload.data.body,
-        icon: payload.notification.icon,
-    };
-
-    self.registration.showNotification(notificationTitle, notificationOptions);
-});
+firebase.messaging();
