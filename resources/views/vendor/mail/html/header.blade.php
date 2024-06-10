7ddb2747b0ea28cb25c55d5da3cf7283bffc4624
Gerard <41292403+gerardpaardenkoper@users.noreply.github.com>
2024-06-10 17:40:12 +0200

    Added some email template customization

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,8 @@
+@props(['url'])
+<tr>
+    <td class="header">
+        <a href="{{ $url }}" style="display: inline-block;">
+            <img src="{{ asset('images/icons/slinc-logo.svg') }}" class="logo" alt="Slinc">
+        </a>
+    </td>
+</tr>
